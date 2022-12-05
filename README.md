3325e4c99129ed06e66561055aa9fa33e7cb07d2
izatwin <88566094+izatwin@users.noreply.github.com>
2022-12-04 20:57:22 -0500

    Grabs data and inputs into dataframe.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Purdue-Algo-Trading
+
+Initially grabs all tickers in the stock market.
+Unfortunately it returns all the results in a dictionary
+inside the dictionary returned. So for each row in the
+dataframe, I took that result and added it to another
+Dataframe.
+
+#### Next Steps:
+I should be able to loop through each ticker, use
+the api to grab whatever piece of data I want and add
+that to another dataframe. From there I'll have to
+decide how to sort and present the data.
---
+++ main.py
@@ -0,0 +1,25 @@
+import polygon
+
+import pandas as pd
+import numpy as np
+
+
+
+
+if __name__ == '__main__':
+    reference_client = polygon.ReferenceClient('ggLrVQdkkqAqKjSZGoEhkgUxY4sJYbik')
+    df = pd.DataFrame(reference_client.get_tickers(market='stocks'))
+
+    cols = ['ticker', 'name', 'market', 'locale', 'primary_exchange', 'type', 'active', 'currency_name', 'cik',
+            'composite_figi', 'share_class_figi', 'last_updated_utc']
+
+    results = pd.DataFrame(columns=cols)
+    results['active'] = results['active'].astype(bool)
+
+    for row in df.itertuples():
+        single_result = pd.DataFrame(data=row.results, index = [0])
+        single_result['active'].astype('bool')
+        results = pd.concat([results, single_result])
+
+
+    print(results)
